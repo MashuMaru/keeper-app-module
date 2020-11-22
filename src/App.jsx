d3016735e40b6e5a5f597138c43d9d70c7db7ea0
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2020-11-22 11:52:36 +0000

    added <Header /> and <Footer /> component + dynanamically updated year

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
 
 function App(){
-    return <h1>Keeper App.</h1>
+    return <div>
+        <Header />
+        <Footer />
+    </div>
 }
 
 export default App;
